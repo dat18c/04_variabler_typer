3dd5b92b713cacae9002b3da62dcb1bae9250e4c
JarlTuxen <jart@kea.dk>
2018-08-30 21:00:07 +0200

    Kode at starte på

---
+++ code/HelloDat18C.java
@@ -0,0 +1,19 @@
+public class HelloDat18C
+{
+   
+   public static void main(String[] args)
+   {
+      //Udskriv noget til skærmen
+      System.out.println("Hello \"Dat18C!\"");
+      System.out.println("Vær velkommen til 1. semester.");
+      System.out.println("Vi klarer os uden bøger de første par dage.");
+      /*
+      meget lang kommentar
+      \t (tab)
+      \" (anførselstegn)
+      \n (ny linie)
+      */
+      System.out.print("et par ord");
+      System.out.print("og en Ferrari");
+   }
+}
